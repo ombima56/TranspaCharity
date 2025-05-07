f9c8917397e0d03fa0e42a7d89406bd37410f42f
Christine Kerubo <82275560+Christine-code-crypto@users.noreply.github.com>
2025-05-07 21:28:17 +0300

    Configure tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'teal': {
+            50: '#f0fdfa',
+            100: '#ccfbf1',
+            200: '#99f6e4',
+            300: '#5eead4',
+            400: '#2dd4bf',
+            500: '#14b8a6',
+            600: '#0d9488',
+            700: '#0f766e',
+            800: '#115e59',
+            900: '#134e4a',
+          },
+          'coral': {
+            50: '#fff1f2',
+            100: '#ffe4e6',
+            200: '#fecdd3',
+            300: '#fda4af',
+            400: '#fb7185',
+            500: '#f43f5e',
+            600: '#e11d48',
+            700: '#be123c',
+            800: '#9f1239',
+            900: '#881337',
+          },
+        },
+        fontFamily: {
+          'sans': ['Inter', 'sans-serif'],
+          'heading': ['Poppins', 'sans-serif'],
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
